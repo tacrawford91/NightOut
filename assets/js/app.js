61afebb8a5e3f8ca0d9c9840217817410f527167
Troy Crawford <Troy@lechler.de>
2018-01-12 07:36:57 -0600

    Added inital folder structure, including firebase setup

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBMPRyBKIBX_3MQAF0oFP8tUlOw0nU0Yn4",
+    authDomain: "project1-d2ee8.firebaseapp.com",
+    databaseURL: "https://project1-d2ee8.firebaseio.com",
+    projectId: "project1-d2ee8",
+    storageBucket: "project1-d2ee8.appspot.com",
+    messagingSenderId: "544845178446"
+  };
+  firebase.initializeApp(config);
+
+
+//   //set up query url
+//   var queryOneURL;
+//   //make api call
+// $.ajax({
+//   url: queryOneURL,
+//   method: "GET"
+// }) .done(function(response){
+//   //generate image for each response
+//  console.log(response.data);
+//   }
+// });
+
+//   //set up query url
+//   var queryTwoURL;
+//   //make api call
+// $.ajax({
+//   url: queryTwoURL,
+//   method: "GET"
+// }) .done(function(response){
+//   //generate image for each response
+//  console.log(response.data);
+//   }
+// });
